f387fdd9bc8a6ab55b481f274df688effed94bdf
rharshini <jeyharshini@yahoo.com>
2016-06-04 23:07:53 +0530

    Add one to counter app

---
+++ Count/app/src/main/java/com/example/dell/count/MainActivity.java
@@ -0,0 +1,53 @@
+package com.example.dell.count;
+
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+    EditText editText;
+    Button button;
+   int a;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        final EditText editText =(EditText) findViewById(R.id.e1);
+        Button button=(Button) findViewById(R.id.b1);
+      button.setOnClickListener(new View.OnClickListener() {
+          @Override
+          public void onClick(View v) {
+              int a =Integer.parseInt(editText.getText().toString());
+              a=a+1;
+              String s=Integer.toString(a);
+              editText.setText(s);
+          }
+      });
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+}
